968918368593b09139fb35167053a9b3899c9093
Ubuntu <ubuntu@ip-172-31-12-215.us-east-2.compute.internal>
2020-04-07 20:04:17 +0000

    first changes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import "./App.css"
+
 class App extends React.Component {
   render () {
     
@@ -12,7 +14,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         {logged_in?<h3><a href={sign_out_route} >Log Out</a></h3>:<h3><a href={sign_in_route} >Log In</a></h3>}
-        Hello World
+        <h1 className="test-one">Hello World</h1>
       </React.Fragment>
     );
   }
