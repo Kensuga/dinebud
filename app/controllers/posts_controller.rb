2049bff8a00341ac993563b5b7116109c8a99fce
Ubuntu <ubuntu@ip-172-31-39-75.us-east-2.compute.internal>
2020-04-07 17:59:17 +0000

    post crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,45 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
-        render json: posts
+        render json: @posts
     end
     
     def show
         @post = Post.find(params[:id])
-        render json: post
+        render json: @post
     end
     
     def create
-        @post = Post.create
-        
+        @post = current_user.posts.create(post_params)
+        if @post.valid?
+            render json: @post
+        else
+            render json: @post.errors
+        end
     end
-    def 
+    
+    def destroy
+        @post = Post.find(params[:id])
+        if @post.destroy
+            render json: @post
+        else 
+            render json: @post.errors
+        end
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+            if @post.valid?
+                render json: @post
+            else
+                render json:@post.errors
+            end
+    end
+    
+    private
+    def post_params
+        params.require(:posts).permit(:location, :schedule_time, :active, :user_id, :partner_id)
+    end
+    
 end
