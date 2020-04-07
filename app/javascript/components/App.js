196991adac8855128c06d24c2208367d0689b4ca
Ubuntu <ubuntu@ip-172-31-41-251.us-east-2.compute.internal>
2020-04-07 20:00:42 +0000

    banner

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {Row, Col} from 'reactstrap'
+import {FaBars} from 'react-icons/fa'
+import './App.css'
+
 class App extends React.Component {
   render () {
     
@@ -11,7 +15,17 @@ class App extends React.Component {
     
     return (
       <React.Fragment>
-        {logged_in?<h3><a href={sign_out_route} >Log Out</a></h3>:<h3><a href={sign_in_route} >Log In</a></h3>}
+      <Row style={{background:"#ffa600", borderBottom: "10px solid #bc7a00"}}>
+        <Col sm={10} style={{display:"flex"}}>
+            <FaBars style={{color:"white", fontSize:"50px", display:"flex",justifyContent:"center"}} />
+          <h1 className={"pacifico"} style={{color:"white", fontSize:"75px",  marginBottom:"0 auto"}}>
+            DineBud
+          </h1>
+        </Col>
+        <Col>
+          {logged_in?<h3><a href={sign_out_route} className={"atma"} style={{color:"white",fontSize:"50px"}}>LogOut</a></h3>:<h3><a href={sign_in_route} className={"atma"} style={{color:"white",fontSize:"50px"}}>LogIn</a></h3>}
+        </Col>
+      </Row>
         Hello World
       </React.Fragment>
     );
