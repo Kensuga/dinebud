9d756ff9f4bf4394ac2748939e55001ff8d401ab
Ubuntu <ubuntu@ip-172-31-41-251.us-east-2.compute.internal>
2020-04-10 17:56:22 +0000

    login fix

--- app/javascript/components/__tests__/HomeTest.js
+++ app/javascript/components/__tests__/HomeTest.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Home from '../pages/Home'
-import App from '../App'
 
 Enzyme.configure({ adapter: new Adapter() })
 
