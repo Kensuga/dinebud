1419bcbf85766a87afe98e60d54aaf7452f84767
Ubuntu <ubuntu@ip-172-31-41-251.us-east-2.compute.internal>
2020-04-10 17:46:36 +0000

    Testing

---
+++ app/javascript/components/__tests__/LoginTest.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Login from '../pages/Login'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+
+it('Login renders without crashing', () => {
+  const div = document.createElement('div')
+  const loginFunction = () => {
+      return true
+  }
+  ReactDOM.render(<Login handleLoginSubmit= { loginFunction }/>, div)
+})
